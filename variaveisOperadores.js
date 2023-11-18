e8b108645eb64f0af37f9ae4cee6b829a4add84b
Jeff <145409374+rivedere@users.noreply.github.com>
2023-11-18 14:09:42 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,13 @@
+
+var x = 5;
+var y = 10;
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
