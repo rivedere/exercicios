73c30b6d97a4fbd9c5aa7c5ee8a5fae7de7481cb
Jeff <145409374+rivedere@users.noreply.github.com>
2023-11-19 18:50:27 -0300

    Add files via upload

---
+++ bissexto.js
@@ -0,0 +1,9 @@
+function bissextoOuNao(ano) {
+    if (ano = 366) {
+        return 'ano é bissexto';
+    }
+    else {
+        return 'ano não é bissexto';
+    }
+}
+console.log(bissextoOuNao(366));
