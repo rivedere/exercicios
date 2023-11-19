9b304effe70c7044600f9981a6835d6dc4b097e2
Jeff <145409374+rivedere@users.noreply.github.com>
2023-11-19 18:52:07 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,9 @@
+function carro() {
+    var cor = 'prata';
+    var modelo = 'celta';
+    var ano = 2010;
+}
+console.log(carro);
+console.log('Cor:', cor);
+console.log('Modelo:', modelo);
+console.log('Ano:', ano);
