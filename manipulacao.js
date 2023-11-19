0629dce1c2850cc6689dc95400ca47ac8848412f
Jeff <145409374+rivedere@users.noreply.github.com>
2023-11-19 18:53:25 -0300

    Add files via upload

---
+++ manipulacao.js
@@ -0,0 +1,14 @@
+function soma() {
+    var numeros = [10, 20, 30];
+
+
+    var numerosSomados = numeros.map(function (numero) {
+        return numero + numero;
+    });
+
+
+    return numerosSomados;
+}
+
+
+
