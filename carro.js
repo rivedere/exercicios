b86167ab4f94bd558632b9f9cdeeef70f99b1d5c
Tiago Bittencourt <121690303+tiago-bitten@users.noreply.github.com>
2023-11-19 23:57:48 -0300

    correcao da correecao da correcao da correcao

--- carro.js
+++ carro.js
@@ -7,3 +7,14 @@ console.log(carro);
 console.log('Cor:', cor);
 console.log('Modelo:', modelo);
 console.log('Ano:', ano);
+
+
+const carro = {
+    cor: 'prata',
+    modelo: 'celta',
+    ano: 2010
+};
+
+console.log(`Cor: ${carro.cor}`)
+console.log(`Modelo: ${carro.modelo}`)
+console.log(`Ano: ${carro.ano}`)
