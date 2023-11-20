893c1adfaa6961454d71de4dc30c194d2bb4ef53
Tiago Bittencourt <121690303+tiago-bitten@users.noreply.github.com>
2023-11-20 00:37:45 -0300

    simfdsfdsd

--- paramentros.js
+++ paramentros.js
@@ -3,3 +3,16 @@ function parametro(y, x) {
     return resultado;
 } 
 var resultadoDaFuncao = parametro(12, 10);
+
+
+
+// apenas um ajuste pessoal
+const soma = (args0, args1) =>
+{
+    return args0 + args1;
+}
+
+const somaDosParametros = soma(5, "Beterraba");
+console.log(somaDosParametros);
+
+// nesse caso, a soma nao quer dizer que é apenas uma operação matematica, pode ser a soma de duas strings, que seria a concatenação
